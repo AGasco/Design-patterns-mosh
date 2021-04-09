4a7d333996de3e87c28c50fc2e6adb4fd464e7df
Antonio Gasco <antoniofgasco@gmail.com>
2021-04-09 12:26:58 +0100

    state-pattern

--- src/com/codewithmosh/state/Tool.java
+++ src/com/codewithmosh/state/Tool.java
@@ -1,10 +1,6 @@
 package com.codewithmosh.state;
 
 public abstract class Tool {
-    public ToolType type;
     public abstract void mouseDown();
     public abstract void mouseUp();
-    public ToolType getType(){
-        return type;
-    }
 }
